ab8eb79ccd51dff7b7460ada955f9b85665166f8
DanielEsso <essodaniel55@gmail.com>
2025-06-18 03:29:49 +0100

    amelioration du css

--- Projet-bibliotheque-user/user_librairy_project/settings.py
+++ Projet-bibliotheque-user/user_librairy_project/settings.py
@@ -57,7 +57,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'librairy_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
